57de30e645bae73baeeb7e578eadceb10e5f4dd4
Andreas Maier <andreas.martin.maier@desy.de>
2022-09-23 16:51:13 +0200

    build and link bundled nauty/Traces library by default

--- build.rs
+++ build.rs
@@ -2,11 +2,22 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rustc-link-lib=nauty");
-    println!("cargo:rerun-if-changed=wrapper.h");
+
+    #[cfg(feature = "bundled")]
+    compile_nauty();
+
+    let wrapper;
+    if cfg!(feature = "bundled") {
+        println!("cargo:rustc-link-lib=nauty_bundled");
+        wrapper = "wrapper-bundled.h";
+    } else {
+        println!("cargo:rustc-link-lib=nauty");
+        wrapper = "wrapper.h";
+    };
+    println!("cargo:rerun-if-changed={wrapper}");
 
     let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
+        .header(wrapper)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .derive_eq(true)
         .derive_hash(true)
@@ -67,4 +78,45 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+
+}
+
+#[cfg(feature = "bundled")]
+fn compile_nauty() {
+    const NAUTY_DIR: &str = "nauty27r4";
+    const NAUTY_HEADERS: [&str; 5] = [
+        "nauty-h.in",
+        "schreier.h",
+        "naugroup.h",
+        "nausparse.h",
+        "traces.h",
+    ];
+    const NAUTY_SRC_FILES: [&str; 15] = [
+        "nauty.c",
+        "nautil.c",
+        "nausparse.c",
+        "naugraph.c",
+        "schreier.c",
+        "naurng.c",
+        "traces.c",
+        "gtools.c",
+        "naututil.c",
+        "nautinv.c",
+        "gutil1.c",
+        "gutil2.c",
+        "gtnauty.c",
+        "naugroup.c",
+        "nautycliquer.c",
+    ];
+
+    for file in NAUTY_HEADERS.iter().chain(NAUTY_SRC_FILES.iter()) {
+        println!("cargo:rerun-if-changed=src/{NAUTY_DIR}/{file}");
+    }
+    let mut cc_cmd = cc::Build::new();
+    cc_cmd.warnings(false).files(
+        NAUTY_SRC_FILES.iter().map(|f| PathBuf::from_iter(["src", NAUTY_DIR, f]))
+    );
+
+    cc_cmd.compile("nauty_bundled");
+
 }
---
+++ wrapper-bundled.h
@@ -0,0 +1,4 @@
+#include "src/nauty27r4/nauty.h"
+#include "src/nauty27r4/naugroup.h"
+#include "src/nauty27r4/nausparse.h"
+#include "src/nauty27r4/traces.h"
